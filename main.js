1a08caf2b92283b9e91221432b48f36de1f90bfb
Lukas Gaucas <34185919+projektorius96@users.noreply.github.com>
2023-06-12 17:45:30 +0100

    init codebase

---
+++ main.js
@@ -0,0 +1,44 @@
+import Konva from 'konva';
+import { SVG } from '@svgdotjs/svg.js'
+
+EventTarget.prototype.on = EventTarget.prototype.addEventListener;
+
+let SOURCE = null;
+
+// first we need to create a stage
+var stage = new Konva.Stage({
+  container: 'app',   // id of container <div>
+  width: window.innerWidth,
+  height: window.innerHeight
+});
+
+// then create layer
+var layer = new Konva.Layer();
+
+var image = null;
+if (SOURCE !== null){
+  image = Konva.Image.fromURL(SOURCE, (image) => {
+    layer.add(image);
+  });
+}
+
+
+// add the shape to the layer
+if (image !== null){
+  layer.add(image);
+}
+
+// add the layer to the stage
+stage.add(layer);
+
+// draw the image
+layer.draw();
+
+document.getElementById('app').contentEditable = true;
+var draw = SVG().addTo(document.getElementById('app')).size(window.innerWidth, window.innerHeight)
+draw.text("I love St.Mocha").move(window.innerWidth / 2 ,window.innerHeight / 2).font({ fill: '#f06', size: "32px", family: 'Inconsolata' })
+document.getElementById('app').on('input', function(){
+  // credits to @https://github.com/konvajs/react-konva/issues/441
+  SOURCE = 'data:image/svg+xml;base64,' + window.btoa(this.firstElementChild.instance.node)
+  /* console.log(SOURCE); */
+})
