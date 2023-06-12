3ff85989a360089f5ae33d2d0fc130a335d8daaa
Lukas Gaucas <34185919+projektorius96@users.noreply.github.com>
2023-06-12 17:53:02 +0100

    follow-up p.2

--- main.js
+++ main.js
@@ -34,11 +34,15 @@ stage.add(layer);
 // draw the image
 layer.draw();
 
-document.getElementById('app').contentEditable = true;
-var draw = SVG().addTo(document.getElementById('app')).size(window.innerWidth, window.innerHeight)
+document.getElementById('svg-layer').contentEditable = true;
+var draw = SVG().addTo(document.getElementById('svg-layer')).size(window.innerWidth, window.innerHeight)
 draw.text("I love St.Mocha").move(window.innerWidth / 2 ,window.innerHeight / 2).font({ fill: '#f06', size: "32px", family: 'Inconsolata' })
 document.getElementById('app').on('input', function(){
   // credits to @https://github.com/konvajs/react-konva/issues/441
   SOURCE = 'data:image/svg+xml;base64,' + window.btoa(this.firstElementChild.instance.node)
   /* console.log(SOURCE); */
+})
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  window.GLOBALS__KONVA_STAGE = stage;
 })
